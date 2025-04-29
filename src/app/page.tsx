cd8ca75e2904f43c5f23b2e061a8cf7bbd272a92
Katherine Szelag <katherine.szelag@alum.utoronto.ca>
2025-04-28 22:48:24 -0400

    fix: centering coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,16 +13,21 @@ export default function Home() {
       {/* top-left name */}
       <div className="absolute top-0 left-0 pt-8 p-4 z-10">
         <div className="flex flex-col">
-        <span className="text-neutral-600 text-2xl font-bold">DAKOTA ARSENEAULT</span>
-        <span className="text-neutral-600 text-lg font-bold">VIDEO EDITOR</span>
-        <span className="text-neutral-600 text-lg font-bold">MOTION DESIGNER</span>
-
+          <span className="text-neutral-600 text-xl md:text-2xl font-bold">
+            DAKOTA ARSENEAULT
+          </span>
+          <span className="text-neutral-600 text-base md:text-lg font-bold">
+            VIDEO EDITOR
+          </span>
+          <span className="text-neutral-600 text-base md:text-lg font-bold">
+            MOTION DESIGNER
+          </span>
         </div>
       </div>
 
       {/* center “Coming soon” */}
       <div className="absolute inset-0 flex items-center justify-center z-10">
-        <h1 className="text-white text-5xl font-bold">COMING SOON......</h1>
+        <h1 className="text-white text-4xl md:text-5xl font-bold">COMING SOON......</h1>
       </div>
   </div>
   )
